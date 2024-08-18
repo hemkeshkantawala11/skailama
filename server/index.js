8ba7fd8a9db3bfde4d18cf6714e50fe846895c3d
hemkesh <hemkesh123kantawala@gmail.com>
2024-08-18 22:09:05 +0530

    CHanged url

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const passport = require("passport");
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Frontend URL
+    origin: 'https://skailama-rust.vercel.app/', // Frontend URL
     credentials: true
 }));
 
